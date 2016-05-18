f880a7357c14cda6f533e7b06927ec0df95318a2
D.Kirichik <d.kirichik@mail.ru>
2016-05-18 23:05:16 +0300

    gitignore changed

---
+++ src/main/java/base/WebSocketService.java
@@ -0,0 +1,7 @@
+package base;
+
+/**
+ * Created by seven-teen on 17.05.16.
+ */
+public class WebSocketService {
+}
---
+++ src/main/java/frontend/GameWebSocket.java
@@ -0,0 +1,7 @@
+package frontend;
+
+/**
+ * Created by seven-teen on 17.05.16.
+ */
+public class GameWebSocket {
+}
