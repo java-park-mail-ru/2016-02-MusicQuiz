f77046e2fe8eec64fdb25e9f13f3f345b8e0971b
nickrom <romanov_niki@mail.ru>
2016-05-19 00:16:43 +0300

    Add new branch

--- src/main/java/frontend/GameWebSocket.java
+++ src/main/java/frontend/GameWebSocket.java
@@ -1,7 +1,12 @@
 package frontend;
 
+
+import org.eclipse.jetty.websocket.api.Session;
+
 /**
  * Created by seven-teen on 17.05.16.
  */
 public class GameWebSocket {
+    private Session session;
+
 }
